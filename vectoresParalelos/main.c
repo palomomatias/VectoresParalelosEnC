1f46825897ab289a40c0058f8de3a57db2e650fe
alumno <alumno>
2018-04-19 11:26:30 -0300

    iniciando los vectores

--- vectoresParalelos/main.c
+++ vectoresParalelos/main.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define TAMANIO 5
+
+
+/*
+nombre puede ser una array de palbra nombre[][50]
+DEFINO no esta en memoria
+
+*/
+
+void mostrarAlumno(char[][50],int[],int[],float[],int[],int);
 
 int main()
 {
-    char nombre[50];
-    int notasUno;
-    int notasDos;
-    float promedios;
+    char nombre[TAMANIO][50]={"juan","Maria","Alfredo","Jose","Lili"};
+    int notasUno[TAMANIO]={};
+    int notasDos[TAMANIO]={};
+    float promedios[TAMANIO]={};
+    int legajo[TAMANIO]={};
+    mostrarAlumno(nombre,notasUno,notasDos,promedios,legajo,TAMANIO);
 
 
     return 0;
+}
+
+
+void mostrarAlumno(char nombres[][50],int primeraNota[],int segundaNota[],float promedio[],int legajos[],int largo)
+{
+    int i;
+    for(i=0;i<largo;i++)
+    {
+        printf("\nL:%d, N:%s,N1:%d,N2:%d,P:%.2f",legajos[i],nombres[i],primeraNota[i],segundaNota[i],promedio[i]);
+    }
 }
